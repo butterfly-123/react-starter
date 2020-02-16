6d0fecaddbab573a48a9084cf58bb243b8753f2b
Bombel <bombel@bombel.com>
2020-02-16 20:32:19 +0100

    Add PropTypes

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,8 +2,14 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
+import PropTypes from 'prop-types';
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired
+  }
+
   render() {
     return (
       <section className={styles.component}>
