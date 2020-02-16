e928b800bfc46f88f309bd302527a1507227a89e
Bombel <bombel@bombel.com>
2020-02-16 22:19:49 +0100

    Add Column and three columns with title

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import styles from './App.scss';
 import List from '../List/List';
-
+import Hero from '../Hero/Hero';
 
 class App extends React.Component {
   render() {
     return (
       <div id="app">
-        <mein className={styles.component}>
+        <div className={styles.component}>
           <h1 className={styles.title}>My first React app</h1>
           <h2 className={styles.subtitle}>Hello world!</h2>
-        </mein>
-        <List title={['Things to do ', <sup key='1'>soon!</sup>]}>
-          <p>I'm planning on doing all these things sooner, rather than later!</p>
-        </List>
+        </div>
+        <List 
+          title={['Things to do ', <sup key='1'>soon!</sup>]} 
+          imgSrc={'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'}
+        />
       </div>
     )
   }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,19 +3,31 @@ import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
+import Column from '../Column/Column';
 
 class List extends React.Component {
   static propTypes = {
     title: PropTypes.node.isRequired,
+    imgSrc: PropTypes.node.isRequired,
     children: PropTypes.node.isRequired
   }
 
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  }
+  
   render() {
     return (
       <section className={styles.component}>
-        <Hero titleText={this.props.title} />
+        <Hero titleText={this.props.title} imgSrc={this.props.imgSrc} />
+        <b>{this.props.aneta}</b>
         <div className={styles.description}>
           {this.props.children}
+        </div>
+        <div className={styles.columns}>
+          <Column title={'Animals'} />
+          <Column title={'Plants'} />
+          <Column title={'Minerals'} />
         </div>
       </section>
     )
