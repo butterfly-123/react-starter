e7d7a02decc680e6997d77ac3e6d84ce37ae5891
Bombel <bombel@bombel.com>
2020-02-16 19:59:23 +0100

    Add App.js react

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import styles from './App.scss';
+import List from '../List/List';
+
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>My first React app trompeta</h1>
+      <div id="app">
+        <mein className={styles.component}>
+          <h1 className={styles.title}>My first React app</h1>
+          <h2 className={styles.subtitle}>Hello world!</h2>
+        </mein>
+        <List /> 
       </div>
     )
   }
