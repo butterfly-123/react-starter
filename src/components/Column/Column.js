9d6c6a0e0b3adb8f0532adc68297451e39720b09
Bombel <bombel@bombel.com>
2020-02-18 14:52:06 +0100

    Make Cart and Column

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,15 +1,63 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
+import {settings} from '../../data/dataStore';
+import Creator from '../Creator/Creator';
+import Card from '../Card/Card';
+import Icon from '../Icon/Icon';
 
-const Column = props => (
-  <section className={styles.component}>
-    <h3 className={styles.title}>{props.title}</h3>
-  </section>
-);
 
-Column.propTypes = {
-  title: PropTypes.node.isRequired,
-};
+
+class Column extends React.Component {
+
+  static propTypes = {
+    title : PropTypes.node.isRequired,
+  }
+
+  static defaultProps = {
+    icon: settings.defaultColumnIcon,
+  }
+
+  state = {
+    cards: this.props.cards || [],
+  }
+  
+  addCard(title){
+    this.setState(state => (
+      {
+        cards: [
+          ...state.cards,
+          {
+            key: state.cards.length ? state.cards[state.cards.length - 1].key + 1 : 0,
+            title,
+            icon: 'list-alt',
+            cards: []
+          }
+        ]
+      }
+    ));
+  }
+
+  
+  
+  render() {
+    return (
+      <section className={styles.component}>
+        <h3 className={styles.title}>{this.props.title}</h3>
+        <span className={styles.icon}>
+            <Icon name={this.props.icon}/>
+        </span>
+        <div>
+          {this.state.cards.map(({key, ...cardsProps}) => (
+            <Card key={key} {...cardsProps} />
+          ))}
+        </div>
+        <div>
+            <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
+          </div>
+      </section>
+    )
+  }
+}
 
 export default Column;
