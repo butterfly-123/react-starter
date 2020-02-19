16562f84f8e5e4e5bfbca0213d8c9c6fa8df5a16
Bombel <bombel@bombel.com>
2020-02-19 11:31:31 +0100

    Change test commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,4 @@ import './styles/global.scss';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
-const test = "1"
-
 ReactDOM.render(<App />, document.getElementById('app'));
