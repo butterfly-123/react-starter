4f7853a74f41f39a4e2e704dc016916a2b7f8494
Bombel <bombel@bombel.com>
2020-02-20 19:58:34 +0100

    Make

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
-import {settings} from '../../data/dataStore';
-import Creator from '../Creator/Creator';
-import Card from '../Card/Card';
+// import {settings} from '../../data/dataStore';
+// import Creator from '../Creator/Creator';
+// import Card from '../Card/Card';
 import Icon from '../Icon/Icon';
 
 
@@ -12,31 +12,37 @@ class Column extends React.Component {
 
   static propTypes = {
     title : PropTypes.node.isRequired,
-    cards: PropTypes.node.isRequired,
+    // cards: PropTypes.node.isRequired,
     icon:  PropTypes.node.isRequired,
   }
 
-  state = {
-    cards: this.props.cards || [],
-  }
+  /*
+    state = {
+      cards: this.props.cards || [],
+    }
+  */
   
-  addCard(title){
-    this.setState(state => (
-      {
-        cards: [
-          ...state.cards,
-          {
-            key: state.cards.length ? state.cards[state.cards.length - 1].key + 1 : 0,
-            title,
-            icon: 'list-alt',
-            cards: [],
-          },
-        ],
-      }
-    ));
-  }
+  /*
+    addCard(title){
+      this.setState(state => (
+        {
+          cards: [
+            ...state.cards,
+            {
+              key: state.cards.length ? state.cards[state.cards.length - 1].key + 1 : 0,
+              title,
+              icon: 'list-alt',
+              cards: [],
+            },
+          ],
+        }
+      ));
+    }
+  */
   
   render() {
+    const {title, icon} = this.props;
+
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>{this.props.title} 
@@ -44,14 +50,17 @@ class Column extends React.Component {
             <Icon name={this.props.icon}/>
           </span>
         </h3>
-        <div>
-          {this.state.cards.map(({key, ...cardsProps}) => (
-            <Card key={key} {...cardsProps} />
-          ))}
-        </div>
-        <div>
-          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
-        </div>
+
+        {/*
+          <div>
+            {this.state.cards.map(({key, ...cardsProps}) => (
+              <Card key={key} {...cardsProps} />
+            ))}
+          </div>
+          <div>
+            <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
+          </div>
+        */}
       </section>
     );
   }
