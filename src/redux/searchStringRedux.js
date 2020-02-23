f2cd8c06e4e4e9ee0f8be5cf5d4f8e070a252eba
Bombel <bombel@bombel.com>
2020-02-23 22:30:53 +0100

    Make searchStringRedux.js

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -2,17 +2,23 @@
 export const getSearchString = ({searchString}) => searchString;
 
 export const countAllCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
+
 // action name creator
+const reducerName = 'search';
+const createActionName = name => `app/${reducerName}/${name}`;
 
 // actions types
+export const SEARCH = createActionName('SEARCH');
 
 // action creators
+export const createAction_changeSearchString = payload => ({ payload, type: SEARCH });
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
+    case SEARCH:
+      return action.payload;
     default:
       return statePart;
   }
 }
-
