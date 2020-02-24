35a7ba20452514f6e183ae51b571f8b022803914
Bombel <bombel@bombel.com>
2020-02-24 09:31:04 +0100

    Change name ADD_SEARCH

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -1,22 +1,23 @@
 // selectors
 export const getSearchString = ({searchString}) => searchString;
 
-export const countAllCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
+export const countAllCards = ({cards, searchString}) =>
+  cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
 
 // action name creator
 const reducerName = 'search';
 const createActionName = name => `app/${reducerName}/${name}`;
 
 // actions types
-export const SEARCH = createActionName('SEARCH');
+export const ADD_SEARCH = createActionName('ADD_SEARCH');
 
 // action creators
-export const createAction_changeSearchString = payload => ({ payload, type: SEARCH });
+export const createAction_changeSearchString = payload => ({ payload, type: ADD_SEARCH });
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
-    case SEARCH:
+    case ADD_SEARCH:
       return action.payload;
     default:
       return statePart;
